3d6d2d0e7a05bca67247fbc0d1a165a639c1d0b7
Birleyici <davudtevhid@gmail.com>
2023-09-11 22:14:12 +0300

    şlkşlk

--- pages/kategori.vue
+++ pages/kategori.vue
@@ -1,16 +1,16 @@
 <template>
-  <div class="px-x-mobil lg:px-x-desktop grid grid-cols-10 gap-5">
+  <div class="px-x-mobil lg:px-x-desktop lg:grid grid-cols-10 gap-5">
     <div class="col-span-2">
       <b>Filtreler</b>
-      <div class="rounded-md bg-white pt-minimal">
-        <div class="border-b p-2 bg-tertiary-100 rounded-t-md">Marka</div>
-        <div class="p-2 bg-tertiary-50 rounded-b-md grid gap-2">
-          <div class="space-x-4"><input type="checkbox" /> Beyraha</div>
-
-          <div><input type="checkbox" /> Ahkaf</div>
+      <div class="rounded-md bg-white pt-minimal w-full">
+        <div class="border py-2 px-4 bg-tertiary-50 rounded-t-md font-medium">Marka</div>
+        <div class="p-4 border-l border-r border-b rounded-b-md grid gap-2">
+          <UiFormCheckbox v-for="item in 4">Marka {{ item }}</UiFormCheckbox>
         </div>
       </div>
     </div>
-    <div class="col-span-8">2</div>
+    <div class="col-span-8">
+      <!-- içerik -->
+    </div>
   </div>
 </template>
