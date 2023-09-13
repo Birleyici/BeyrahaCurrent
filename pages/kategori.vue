2b8d9680970b1c192485906f80ebdc514af0db66
Birleyici <davudtevhid@gmail.com>
2023-09-13 08:10:58 +0300

    lkşl

--- pages/kategori.vue
+++ pages/kategori.vue
@@ -1,16 +1,88 @@
 <template>
-  <div class="px-x-mobil lg:px-x-desktop lg:grid grid-cols-10 gap-5">
-    <div class="col-span-2">
+  <div class="px-x-mobil lg:px-x-desktop lg:grid grid-cols-10 gap-10">
+    
+    <button class="w-full bg-slate-50 border rounded-full mb-minimal py-2 text-secondary-500 flex items-center space-x-2 justify-center">
+    <Icon name="ic:round-filter-alt" class="w-5 h-5"></Icon>
+    <p class="font-medium">Filtreler</p>
+    </button>
+    <div v-if="false" class="col-span-2">
       <b>Filtreler</b>
-      <div class="rounded-md bg-white pt-minimal w-full">
-        <div class="border py-2 px-4 bg-tertiary-50 rounded-t-md font-medium">Marka</div>
-        <div class="p-4 border-l border-r border-b rounded-b-md grid gap-2">
-          <UiFormCheckbox v-for="item in 4">Marka {{ item }}</UiFormCheckbox>
+      <div class="space-y-minimal">
+        <div class="rounded-md bg-white pt-minimal w-full">
+          <div
+            @click="isOpen = !isOpen"
+            :class="isOpen ? 'rounded-t-md' : 'rounded-md'"
+            class="no-select cursor-pointer border py-2 px-4 bg-tertiary-50 font-medium flex justify-between items-center"
+          >
+            <p>Marka</p>
+            <Icon
+              v-if="isOpen"
+              name="material-symbols:keyboard-arrow-down"
+              class="w-4 h-4"
+            ></Icon>
+            <Icon
+              v-else
+              name="material-symbols:keyboard-arrow-right"
+              class="w-4 h-4"
+            ></Icon>
+          </div>
+          <div
+            :class="isOpen ? 'border-l border-r border-b max-h-[200px] p-4' : 'max-h-0'"
+            class="duration-300 rounded-b-md grid gap-2 overflow-hidden overflow-y-scroll"
+          >
+            <UiFormInput placeholder="Marka ara"></UiFormInput>
+            <UiFormCheckbox v-for="item in 12">Marka {{ item }}</UiFormCheckbox>
+          </div>
         </div>
+        <label class="relative inline-flex items-center cursor-pointer">
+          <input type="checkbox" value="" class="sr-only peer" />
+          <div
+            class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-0 border-none after:border-none before:border-none peer-focus:ring-secondary-300 dark:peer-focus:ring-secondary-500 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-secondary-500"
+          ></div>
+          <span class="ml-3 text-sm font-medium text-gray-900">Ücretsiz kargo</span>
+        </label>
       </div>
     </div>
-    <div class="col-span-8">
-      <!-- içerik -->
+    <div class="col-span-8 space-y-minimal">
+      <div class="space-y-minimal lg:flex justify-between items-center">
+        <div class="flex space-x-4 items-center">
+          <div class="flex space-x-2">
+            <div class="bg-slate-100 rounded-full p-2 text-sm flex space-x-2">
+              <span>Ücretsiz Kargo</span>
+              <button
+                class="rounded-full bg-slate-300 w-5 h-5 flex items-center justify-center"
+              >
+                <Icon name="mdi:close"></Icon>
+              </button>
+            </div>
+          </div>
+          <div class="flex space-x-2">
+            <div class="bg-slate-100 rounded-full p-2 text-sm flex space-x-2">
+              <span>Marka 1</span>
+              <button
+                class="rounded-full bg-slate-300 w-5 h-5 flex items-center justify-center"
+              >
+                <Icon name="mdi:close"></Icon>
+              </button>
+            </div>
+          </div>
+        </div>
+        <select
+          class="w-72 bg-slate-50 text-sm focus:bg-white duration-300 p-3 focus:!outline-0 focus:!ring-0 focus:border-secondary-500 w-full rounded-md border"
+        >
+          <option value="">Akıllı sıralama</option>
+          <option value="">En çok satanlar</option>
+          <option value="">Azalan fiyat</option>
+          <option value="">Artan fiyat</option>
+        </select>
+      </div>
+      <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5">
+        <PartialsProductCard v-for="item in 15"></PartialsProductCard>
+      </div>
     </div>
   </div>
 </template>
+
+<script setup>
+const isOpen = ref(true);
+</script>
