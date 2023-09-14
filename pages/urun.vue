c6a072b42b0934a5e8c8b60ff1d01f9fc8a10f41
Birleyici <davudtevhid@gmail.com>
2023-09-14 11:31:49 +0300

    lkşlk

--- pages/urun.vue
+++ pages/urun.vue
@@ -181,26 +181,26 @@
     </div>
 
     <div class="px-6 lg:px-0 mt-minimal lg:mt-maximal">
-      <p class="mb-2 font-bold">Yorumlar</p>
-      <div class="bg-tertiary-100 items-stretch rounded-md flex  items-center">
-    <div class="bg-tertiary-200  rounded-l-md flex items-center p-2">
-        <Icon name="ph:info" class="w-8 h-8 mx-auto"></Icon>
-    </div>
-    <div class="p-4">
-        Ürüne yalnızca ürünü satın alan oturum açmış kullanıcılar yorum yapabilir.
-    </div>
-</div>
+      <p class="mb-2 font-bold">Yorum yap</p>
+      <div class="bg-tertiary-100 items-stretch rounded-md flex items-center">
+        <div class="bg-tertiary-200 rounded-l-md flex items-center p-2">
+          <Icon name="ph:info" class="w-8 h-8 mx-auto"></Icon>
+        </div>
+        <div class="p-4">
+          Ürüne yalnızca ürünü satın alan oturum açmış kullanıcılar yorum yapabilir.
+        </div>
+      </div>
 
       <UiFormTextarea
         class="h-60 my-minimal border-quaternary-300"
         placeholder="Yorumunuz"
       ></UiFormTextarea>
 
-      
-
-      <div class="mb-minimal space-y-minimal lg:space-y-0 lg:flex items-center justify-between">
+      <div
+        class="mb-minimal space-y-minimal lg:space-y-0 lg:flex items-center justify-between"
+      >
         <UiFormFileInput></UiFormFileInput>
-        <UiFormSelect class="!w-full lg:!w-72 ">
+        <UiFormSelect class="!w-full lg:!w-72">
           <option value="">Ürüne puanınız</option>
           <option class="text-secondary-500" value="">★</option>
           <option class="text-secondary-500" value="">★★</option>
@@ -210,6 +210,35 @@
         </UiFormSelect>
       </div>
       <UiButtonsBaseButton color="secondary">Yorumu gönder</UiButtonsBaseButton>
+    </div>
+
+    <div class="px-6 lg:px-0 mt-minimal lg:mt-maximal">
+      <p class="mb-2 font-bold">Yorumlar</p>
+
+      <div class="grid gap-4 border-b border-dotted pb-3">
+        <div class="space-y-4">
+          <div class="flex space-x-2">
+            <p class="font-medium">Muhammet Ç*** Y***</p>
+            <div class="flex">
+              <Icon
+                v-for="item in 5"
+                name="material-symbols:star-rate-rounded"
+                fill
+                class="text-secondary-500"
+              ></Icon>
+            </div>
+          </div>
+          <div>
+            <img src="/default-product.jpg" class="w-16 rounded-md" alt="" />
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et odio neque,
+            officia iste ratione veniam. Laboriosam possimus aut earum vero ducimus
+            praesentium sed debitis, sapiente illo harum quidem magni voluptatum!
+          </p>
+          <p class="text-sm italic">23 Eylül 2023 tarihinde satın alındı</p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
