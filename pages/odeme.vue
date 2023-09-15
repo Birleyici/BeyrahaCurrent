1277608ad12c0c86e68dd8dc398f51eb402df7d6
Birleyici <davudtevhid@gmail.com>
2023-09-15 07:03:17 +0300

    odeme sayfası geliştirmeleri

--- pages/odeme.vue
+++ pages/odeme.vue
@@ -2,16 +2,18 @@
   <div class="px-x-mobil lg:px-x-desktop">
     <div class="grid lg:grid-cols-3 gap-16">
       <div class="col-span-2">
-        <b>Sipariş oluştur</b>
+        <div class="text-center lg:text-left">
+          <b>Sipariş oluştur</b>
+        </div>
 
         <div class="border rounded-md mt-minimal">
           <div class="bg-slate-50 p-minimal rounded-t-md flex items-center">
-            <UiFormRadio :checked="true">
+            <UiFormRadio v-if="true" :checked="true">
               <div class="lg:flex items-center space-x-4">
                 <div></div>
 
                 <div class="space-y-1">
-                  <div class="flex space-x-2 ">
+                  <div class="flex space-x-2">
                     <p>
                       <span class="font-medium !bg-transparent">İsim:</span> Muhammet
                       Çağrı Yılmaz
@@ -32,21 +34,93 @@
                 </div>
               </div>
             </UiFormRadio>
+            <div v-else>Henüz kayıtlı adresiniz bulunmuyor.</div>
           </div>
 
           <div class="flex justify-between border-t">
             <div class="p-3 flex items-center space-x-2 text-secondary-500 text-sm">
-              <p class="font-medium">Yeni adres</p>
-              <Icon name="mdi:plus" class="font-medium"></Icon>
+              <button @click="isOpen = true" class="font-semibold">Yeni adres</button>
+              <Icon name="mdi:plus" class="font-semibold w-4 h-4"></Icon>
             </div>
-            <div class="flex justify-center space-x-2 p-3  flex items-center">
-              <p class="text-sm font-medium">Diğer adresleri gör</p>
-              <Icon name="material-symbols:keyboard-arrow-down" class="font-medium"></Icon>
+            <div class="flex justify-center space-x-2 p-3 flex items-center">
+              <p class="text-sm font-semibold">Diğer adresleri gör</p>
+              <Icon
+                name="material-symbols:keyboard-arrow-down"
+                class="font-medium w-5 h-5"
+              ></Icon>
             </div>
           </div>
         </div>
       </div>
       <div class="col-span-1">1</div>
     </div>
+
+    <div v-if="isOpen">
+      <div
+        class="p-minimal lg:rounded-md space-y-4 fixed top-0 bottom-0 right-0 left-0 h-full lg:h-5/6 w-full mx-auto my-auto bg-white z-10 max-w-[500px] overflow-scroll-y"
+      >
+        <b>Yeni adres</b>
+
+        <div class="grid gap-4 grid-cols-2">
+          <div>
+            <label for="" class="text-sm">Ad* </label>
+            <UiFormInput></UiFormInput>
+          </div>
+          <div>
+            <label for="" class="text-sm">Soyad*</label>
+            <UiFormInput></UiFormInput>
+          </div>
+        </div>
+        <div class="grid gap-4 grid-cols-2">
+          <div>
+            <label for="" class="text-sm">Telefon* </label>
+            <UiFormInput></UiFormInput>
+          </div>
+          <div>
+            <label for="" class="text-sm">İl*</label>
+            <UiFormSelect>
+              <option value="">İl seçin</option>
+            </UiFormSelect>
+          </div>
+        </div>
+        <div class="grid gap-4 grid-cols-2">
+          <div>
+            <label for="" class="text-sm">İlçe* </label>
+            <UiFormSelect>
+              <option value="">İlçe seçin</option>
+            </UiFormSelect>
+          </div>
+          <div>
+            <label for="" class="text-sm">Mahalle*</label>
+            <UiFormSelect>
+              <option value="">Mahalle seçin</option>
+            </UiFormSelect>
+          </div>
+        </div>
+        <div>
+          <label for="" class="text-sm">Adres* </label>
+          <UiFormTextarea></UiFormTextarea>
+        </div>
+        <div>
+          <label for="" class="text-sm">Adres başlığı* </label>
+          <UiFormInput></UiFormInput>
+        </div>
+
+        <UiButtonsBaseButton color="secondary" class="px-6 font-medium"
+          >Adresi kaydet</UiButtonsBaseButton
+        >
+      </div>
+      <button class="fixed right-0 top-0 z-[10]" @click="isOpen = false">
+        <Icon name="mdi:close" class="text-tertiary-50 w-20 h-20"></Icon>
+      </button>
+      <div
+        @click="isOpen = false"
+        class="fixed left-0 right-0 bottom-0 left-0 bg-black w-full h-full z-[4] opacity-30"
+      ></div>
+    </div>
   </div>
 </template>
+
+<script setup>
+const isOpen = ref(false);
+</script>
