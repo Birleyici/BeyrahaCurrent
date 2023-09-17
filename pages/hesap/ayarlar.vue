d76967fe413d25cab57d9530ebed174bd30f69e9
Birleyici <davudtevhid@gmail.com>
2023-09-17 13:46:58 +0300

    lklkjkl

---
+++ pages/hesap/ayarlar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="px-x-mobil lg:px-x-desktop my-minimal">
+    <b>Adreslerim</b>
+
+    <div class="lg:grid lg:grid-cols-4 mt-minimal lg:mt-orta gap-10 space-y-minimal">
+      <div class="col-span-1">
+        <PartialsAccountMenu></PartialsAccountMenu>
+      </div>
+      <div class="col-span-3">
+       Ayarlar
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const onayla = ref(false);
+const confirmDefaultModal = ref(false);
+const newAddressModal = ref(false);
+const isOpen = ref(false);
+</script>
