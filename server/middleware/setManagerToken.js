f0b80590a6669388518d347b96f6d2381f72badf
Birleyici <davudtevhid@gmail.com>
2023-11-28 09:13:37 +0300

    kljklj

--- server/middleware/setManagerToken.js
+++ server/middleware/setManagerToken.js
@@ -3,8 +3,12 @@ import {getToken} from '#auth'
 export default defineEventHandler(async (event) => {
     console.log(getToken, "gettoken")
 
+  try {
     let token = await getToken({ event })
     console.log(token, "burda")
+  } catch (error) {
+    console.log(error)
+  }
     setCookie(event, 'token', token?.jwt, { httpOnly: true })
 
 });
