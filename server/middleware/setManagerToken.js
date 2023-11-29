95e770e8e83de2c113062434cc27f788339060c2
Birleyici <davudtevhid@gmail.com>
2023-11-29 23:52:33 +0300

    lklkj

--- server/middleware/setManagerToken.js
+++ server/middleware/setManagerToken.js
@@ -5,6 +5,8 @@ export default defineEventHandler(async (event) => {
 
     let jwtEncodedToken = getCookie(event, 'next-auth.session-token') || 0
 
+    console.log(jwtEncodedToken, "jwt encoded token in set manager token")
+
 const token = await decode({
     token: jwtEncodedToken,
     secret: 'gS2v4jn3Rc2qkV5u36vbaW6iYL1805YVIHh2VtiP8HVvXlbM0VKv6MRn4VOKg7A3',
