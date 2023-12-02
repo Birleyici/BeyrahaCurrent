194c746eb3e2f6c1343c1ed723fafe4d4fb3b6bf
Birleyici <davudtevhid@gmail.com>
2023-12-03 01:22:22 +0300

    kjlkj

---
+++ server/api/token.get.js
@@ -0,0 +1,29 @@
+import { decode } from 'next-auth/jwt';
+
+export default defineEventHandler(async (event) => {
+
+
+    let jwtEncodedToken
+
+    if(process.env.NODE_ENV === 'production'){
+
+    jwtEncodedToken = getCookie(event, '__Secure-next-auth.session-token') || 0
+
+    } else {
+
+    jwtEncodedToken = getCookie(event, 'next-auth.session-token') || 0
+
+    }
+
+
+    const token = await decode({
+        token: jwtEncodedToken,
+        secret: 'gS2v4jn3Rc2qkV5u36vbaW6iYL1805YVIHh2VtiP8HVvXlbM0VKv6MRn4VOKg7A3',
+    });
+
+    return token
+
+});
+
+
+
