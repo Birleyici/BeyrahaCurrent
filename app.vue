b652d841768d47e27f35f917b7383b201c567fc4
Birleyici <davudtevhid@gmail.com>
2023-12-06 23:46:35 +0300

    kl

--- app.vue
+++ app.vue
@@ -9,11 +9,27 @@ onMounted(() => {
   useMain().isLoaded = true;
 });
 
-const headers = useRequestHeaders(["cookie"]);
-const { data: token } = await useFetch("/api/token", { headers });
+const bitis = useCookie("bitis") || 0;
 
-if (process.server) {
-  const counter = useCookie("token");
-  counter.value = token?.value?.jwt || null;
+// Cookie'den alınan değeri sayıya dönüştürün
+const bitisTimestamp = parseInt(bitis.value) * 1000;
+
+// Şu anki zamanı alın
+const now = new Date().getTime();
+
+// Bitiş zamanı ile şu anki zamanı karşılaştırın
+if (bitisTimestamp > now) {
+  console.log("Zaman henüz geçmedi");
+} else {
+
+  console.log("burda")
+  const headers = useRequestHeaders(["cookie"]);
+  const { data: token } = await useFetch("/api/token", { headers });
+  if (process.server) {
+    const counter = useCookie("token");
+    const bitis = useCookie("bitis");
+    counter.value = token?.value?.jwt || null;
+    bitis.value = token?.value?.bitis || null;
+  }
 }
 </script>
