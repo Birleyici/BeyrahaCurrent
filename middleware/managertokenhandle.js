a250e710de5c4bf0091eda994568f25e8a357c3c
Birleyici <davudtevhid@gmail.com>
2023-12-06 11:57:37 +0300

    lkjjkl

--- middleware/managertokenhandle.js
+++
@@ -1,8 +0,0 @@
-export default defineNuxtRouteMiddleware(async (to, from) => {
-  // const access_token = useCookie('manager_token')
-  // if (!access_token.value && useRoute().fullPath.indexOf('login') == -1) {
-  //   const headers = useRequestHeaders(['cookie'])
-  //   const { data: token } = await useFetch('/api/token', { headers })
-  //   access_token.value = token.value.jwt
-  // }
-})
