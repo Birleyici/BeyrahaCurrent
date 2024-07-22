66f95730cd66f5ddedd9dde5961cfe32826964f5
Birleyici <davudtevhid@gmail.com>
2024-07-22 13:13:25 +0300

    kjjhhk

--- composables/frontend/product/useProduct.js
+++ composables/frontend/product/useProduct.js
@@ -4,29 +4,28 @@ export function useProduct() {
 
     const productState = useProductState()
 
-    const getProducts = async ({ piece, filters }) => {
-
-
-        
-
+    const getProducts = async ({ piece, filters = {} }) => {
+        // Eğer filters objesinde page yoksa, varsayılan olarak 1 ayarla
+        if (!('page' in filters)) {
+            filters.page = 1;
+        }
+    
         const params = piece ? { piece } : { ...filters };
-
+    
         const { data, error } = await useBaseFetch('products', {
             params
         });
-
+    
         if (data.value && !error.value) {
             // Gelen veri sayfalı ise data.value.data, değilse düz data.value
             productState.$patch({ products: data.value });
         }
-
-
-        if (process.client) {
-
-            localStorage.setItem("prevPage", filters.page)
-
+    
+        if (process.client && 'page' in filters) {
+            localStorage.setItem("prevPage", filters.page);
         }
     };
+    
 
 
 
