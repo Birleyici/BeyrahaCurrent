6b8fdd168d53fa04b610e313efc8b9352fe5d8a5
Birleyici <davudtevhid@gmail.com>
2024-08-21 12:58:10 +0300

    kjhkj

--- components/Ui/slides/UiSlidesProductSlide.vue
+++ components/Ui/slides/UiSlidesProductSlide.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="grid gap-2 mt-minimal lg:mt-maximal" v-if="props.products?.length > 0">
-    <b class="text-md">{{props.title}}</b>
+  <div class="mt-minimal lg:mt-maximal " v-if="props.products?.length > 0">
+    <b class="text-md my-2">{{props.title}}</b>
     <UCarousel arrows v-slot="{ item }" :items="props.products" :ui="{ item: 'snap-end' }">
       <PartialsProductCard class="mr-4" :product="item" :key="item.id"></PartialsProductCard>
     </UCarousel>
@@ -9,4 +9,13 @@
 
 <script setup>
 const props = defineProps(['products', 'title'])
+
+const items = [
+  'https://picsum.photos/600/600?random=1',
+  'https://picsum.photos/600/600?random=2',
+  'https://picsum.photos/600/600?random=3',
+  'https://picsum.photos/600/600?random=4',
+  'https://picsum.photos/600/600?random=5',
+  'https://picsum.photos/600/600?random=6'
+]
 </script>
