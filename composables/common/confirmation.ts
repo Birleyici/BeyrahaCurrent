1714b380c0b6e9c787a48ce0a78f625d03d0cf0a
Birleyici <davudtevhid@gmail.com>
2024-09-02 10:25:16 +0300

    njknjkn

--- composables/common/confirmation.ts
+++ composables/common/confirmation.ts
@@ -1,5 +1,5 @@
 import { openDialog } from 'vue3-promise-dialog';
-import UiModalConfirmation from '@/components/UI/Modal/UiModalConfirmation.vue';
+import UiModalConfirmation from '@/components/Ui/Modal/UiModalConfirmation.vue';
 
 export async function useConfirmation(header?: string, text?: string) {
     return await openDialog(UiModalConfirmation, { header, text });
