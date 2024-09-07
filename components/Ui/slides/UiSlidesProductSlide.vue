675396223a205c57f2219a946d45bf703117e590
Birleyici <davudtevhid@gmail.com>
2024-09-07 10:25:16 +0300

    kbhbhj

--- components/Ui/slides/UiSlidesProductSlide.vue
+++ components/Ui/slides/UiSlidesProductSlide.vue
@@ -1,12 +1,12 @@
 <template>
   <div v-if="products.length > 0">
-   <div class="flex items-center space-x-4">
-    <p class="text-md font-medium my-2">{{ props.title }}</p>
-    <NuxtLink v-if="props.to" :to="props.to" class="flex space-x-1 items-center text-sm font-medium text-orange-500">
-      <span>Tümü</span>
-      <UIcon name="i-heroicons-chevron-right" />
-    </NuxtLink>
-   </div>
+    <div class="flex items-center space-x-4">
+      <p class="text-md font-medium my-2">{{ props.title }}</p>
+      <NuxtLink v-if="props.to" :to="props.to" class="flex space-x-1 items-center text-sm font-medium text-orange-500">
+        <span>Tümü</span>
+        <UIcon name="i-heroicons-chevron-right" />
+      </NuxtLink>
+    </div>
     <UCarousel arrows v-slot="{ item }" :items="products" :ui="{ item: 'snap-end' }">
       <PartialsProductCard class="mr-4" :product="item" :key="item.id"></PartialsProductCard>
     </UCarousel>
@@ -16,11 +16,10 @@
 <script setup>
 const props = defineProps(['title', 'filters', 'to'])
 const productState = useProductState()
-const products = ref([])
 
-onMounted(async () => {
+const { data: products } = await useAsyncData('initIndexPage', async () => {
   const response = await productState.getProducts(props.filters, true)
-  products.value = response
+  return response
 })
 
 </script>
