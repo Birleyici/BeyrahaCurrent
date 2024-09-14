04b57abf5578f11af5c7d7e950344949924f544c
Birleyici <davudtevhid@gmail.com>
2024-09-14 07:19:12 +0300

    kmlkml

--- store/common/auth.js
+++ store/common/auth.js
@@ -1,181 +1,169 @@
-import { defineStore } from "pinia";
+import { defineStore } from 'pinia'
 
-export const useAuthStore = defineStore('authStore', () => {
-    const token = ref('');
+export const useAuthStore = defineStore(
+  'authStore',
+  () => {
+    const token = ref('')
     const orderState = useOrderStoreFront()
     const cartState = useCartState()
 
     const user = ref({
-        email: '',
-        password: ''
+      email: '',
+      password: ''
     })
 
     const register = ref({
-        email: '', 
-        password: '',
-        password_confirmation: ''
+      email: '',
+      password: '',
+      password_confirmation: ''
     })
 
     const apiError = ref({
-        login: [],
-        register: [],
-        remind: []
+      login: [],
+      register: [],
+      remind: []
     })
 
     const anon = ref({
-        id: null,
-        name: null
+      id: null,
+      name: null
     })
 
     const loading = ref({
-        login: false,
-        register: false,
-        remind: false
+      login: false,
+      register: false,
+      remind: false
     })
 
-
-
-
     const login = async () => {
-        loading.value.login = true;
-        const response = await useBaseOFetchWithAuth(
-            "auth/login",
-            {
-                body: JSON.stringify({ ...user.value }),
-                method: "POST",
-                onResponseError: (errorReponse) => {
-                    apiError.value.login = [[errorReponse.response._data.error]]
-                    loading.value.login = false;
-
-                }
-            }
-        ).finally(() => {
-            console.log("deneme")
-            loading.value.login = false;
-        });
-
-
-        if (!response.error) {
-            apiError.value.login = []
-            token.value = response.token
-            actionsOnLogin()
-            return true
-        } else {
-            token.value = null
-            console.log(response.error)
-            return false
+      loading.value.login = true
+      const response = await useBaseOFetchWithAuth('auth/login', {
+        body: JSON.stringify({ ...user.value }),
+        method: 'POST',
+        onResponseError: (errorReponse) => {
+          apiError.value.login = [[errorReponse.response._data.error]]
+          loading.value.login = false
         }
+      }).finally(() => {
+        console.log('deneme')
+        loading.value.login = false
+      })
 
-
-    };
-
+      if (!response.error) {
+        apiError.value.login = []
+        token.value = response.token
+        actionsOnLogin()
+        return true
+      } else {
+        token.value = null
+        console.log(response.error)
+        return false
+      }
+    }
 
     const actionsOnLogin = async () => {
-        await orderState.fetchAddresses()
-        await cartState.cartDBToState()
+      await orderState.fetchAddresses()
+      await cartState.cartDBToState()
     }
 
     const actionsOnLogout = async () => {
-        await orderState.fetchAddresses()
-        await cartState.cartDBToState()
+      await orderState.fetchAddresses()
+      await cartState.cartDBToState()
     }
 
     const registerUser = async () => {
-        loading.value.register = true;
-        const response = await useBaseOFetch(
-            "auth/register",
-            {
-                body: JSON.stringify({ ...register.value }),
-                method: "POST",
-                onResponseError: (errorReponse) => {
-
-                    apiError.value.register = errorReponse.response._data
-                }
-            }
-        ).finally(() => {
-            loading.value.register = false;
-        });
-
-        if (!response.error) {
-            apiError.value.register = null
-            token.value = response.token
-            return true
-        } else {
-            token.value = null
-            return false
+      loading.value.register = true
+      const response = await useBaseOFetchWithAuth('auth/register', {
+        body: JSON.stringify({ ...register.value }),
+        method: 'POST',
+        onResponseError: (errorReponse) => {
+          apiError.value.register = errorReponse.response._data
         }
+      }).finally(() => {
+        loading.value.register = false
+      })
 
-
-    };
-
-    const remind = async (email)=>{
-        loading.value.remind = true
-         await useBaseOFetch(
-            "auth/remind",
-            {
-                body: JSON.stringify({email}),
-                method: "POST",
-                onResponseError: (errorResponse) => {
-                    const data = errorResponse.response._data;
-                    
-                    // Eğer veri bir array ise apiError.value.remind'e ata
-                    if (Array.isArray(data)) {
-                        apiError.value.remind = data;
-                    } else {
-                        // Array değilse, hata mesajını işleyebilir ya da görmezden gelebilirsiniz
-                        console.log('Hata mesajı array formatında değil:', data);
-                    }
-                }
-                
-            }
-        ).finally(() => {
-            loading.value.remind = false;
-        });
+      if (!response.error) {
+        apiError.value.register = null
+        token.value = response.token
+        return true
+      } else {
+        token.value = null
+        return false
+      }
     }
 
+    const remind = async (email) => {
+      loading.value.remind = true
+      await useBaseOFetch('auth/remind', {
+        body: JSON.stringify({ email }),
+        method: 'POST',
+        onResponseError: (errorResponse) => {
+          const data = errorResponse.response._data
+
+          // Eğer veri bir array ise apiError.value.remind'e ata
+          if (Array.isArray(data)) {
+            apiError.value.remind = data
+          } else {
+            // Array değilse, hata mesajını işleyebilir ya da görmezden gelebilirsiniz
+            console.log('Hata mesajı array formatında değil:', data)
+          }
+        }
+      }).finally(() => {
+        loading.value.remind = false
+      })
+    }
 
     const changePassword = async (form) => {
-        loading.value.remind = true;
-        try {
-          const response = await useBaseOFetch("auth/reset-password", {
-            body: JSON.stringify(form),
-            method: "POST",
-          });
-          return response; // Başarılı yanıtı döndürüyoruz
-        } catch (error) {
-          console.error("Error in changePassword", error);
-          throw error; // Hata fırlatıyoruz, böylece `onChange` fonksiyonunda yakalanabilir
-        } finally {
-          loading.value.remind = false;
-        }
-      };
-      
-
-
-    const logout = (callback = null) => {
-
-        token.value = null
-        user.value = {
-            email: '',
-            password: ''
-        }
-        register.value = {
-            email: '',
-            password: '',
-            password_confirmation: ''
-        }
-        actionsOnLogout()
-        
-        callback ? navigateTo(callback) : navigateTo('/auth')
-
+      loading.value.remind = true
+      try {
+        const response = await useBaseOFetch('auth/reset-password', {
+          body: JSON.stringify(form),
+          method: 'POST'
+        })
+        return response // Başarılı yanıtı döndürüyoruz
+      } catch (error) {
+        console.error('Error in changePassword', error)
+        throw error // Hata fırlatıyoruz, böylece `onChange` fonksiyonunda yakalanabilir
+      } finally {
+        loading.value.remind = false
+      }
     }
 
+    const logout = (callback = null) => {
+      token.value = null
+      user.value = {
+        email: '',
+        password: ''
+      }
+      register.value = {
+        email: '',
+        password: '',
+        password_confirmation: ''
+      }
+      actionsOnLogout()
 
-    return { user, anon, register, loading, apiError, token, login, registerUser, remind, logout, changePassword }
-},
-    {
-        persist: {
-            storage: piniaPluginPersistedstate.localStorage(),
-            pick:['token', 'anon']
-        },
-    })
+      callback ? navigateTo(callback) : navigateTo('/auth')
+    }
+
+    return {
+      user,
+      anon,
+      register,
+      loading,
+      apiError,
+      token,
+      login,
+      registerUser,
+      remind,
+      logout,
+      changePassword
+    }
+  },
+  {
+    persist: {
+      storage: piniaPluginPersistedstate.localStorage(),
+      pick: ['token', 'anon']
+    }
+  }
+)
