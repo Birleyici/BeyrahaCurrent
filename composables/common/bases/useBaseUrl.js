a83bdf60802988fc28a7e5dc1e5e4402d8990ddb
Birleyici <davudtevhid@gmail.com>
2025-01-24 12:00:46 +0300

    kljljk

--- composables/common/bases/useBaseUrl.js
+++ composables/common/bases/useBaseUrl.js
@@ -2,6 +2,6 @@ export const useBaseUrl = () => {
   if (process.env.NODE_ENV === 'production') {
     return 'https://api.beyraha.com/api/'
   } else {
-    return 'http://localhost/laravel-api/public/api/'
+    return 'https://api.beyraha.com/api/'
   }
 }
