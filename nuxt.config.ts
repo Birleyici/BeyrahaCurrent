a0933e239035373eed9b495a5b4eae73d255387d
Birleyici <davudtevhid@gmail.com>
2025-01-24 13:16:19 +0300

    jhkj

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -111,15 +111,8 @@ export default defineNuxtConfig({
 
   compatibilityDate: '2024-08-21',
 
-  nitro: {
-    preset: 'cloudflare-pages'
-  },
 
   routeRules: {
     '/management/**': { ssr: false },
-    '/**': { 
-      ssr: true,
-      static: false
-    }
   }
 })
