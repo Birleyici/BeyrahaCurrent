46455790b5c480e229860a38711b32c502956b0b
Birleyici <davudtevhid@gmail.com>
2025-06-19 17:44:46 +0300

    lkjlkj

--- composables/common/bases/useBaseUrl.js
+++ composables/common/bases/useBaseUrl.js
@@ -1,6 +1,6 @@
 export const useBaseUrl = () => {
   if (process.env.NODE_ENV === 'production') {
-    return 'https://api.beyraha.com/api'
+    return 'https://api.beyraha.com/api/'
   } else {
     return 'http://localhost/api/'
   }
